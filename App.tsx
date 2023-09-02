3d8c672cc4398d2b18629060b5aa25fd5e52b5d3
stringstr <>
2023-09-02 08:55:53 +0800

    feat(master): init add

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { TarotCards } from '@/TarotCards';
 
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-slate-400">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TarotCards />
     </View>
   );
 }
